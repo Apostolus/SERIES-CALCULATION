6d8b1c1be2efb7505ca62482cfb7e3fe0200c7e9
Jonathan <jonathan.mboko@gmail.com>
2018-03-20 21:06:30 +0100

    correction valeur de retour de la fonction inverse

--- inversion.py
+++ inversion.py
@@ -7,7 +7,7 @@ X = R.gen()
 def inverse(F,N) :
     #calcul pour une serie formelle de la forme F=1+XG de son inverse modulo N
     if N == 1 :
-        return 1
+        return R(1)
     else :
         G = inverse(F,(N +1)//2)
         return (G+(1-G.multiplication_trunc(F,N)).multiplication_trunc(G,N))
